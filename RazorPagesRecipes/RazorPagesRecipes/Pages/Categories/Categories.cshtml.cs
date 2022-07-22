f6ae14917f7eab1a02ba768224f83f3d35f3db09
dinamaged1 <76622624+dinamaged1@users.noreply.github.com>
2022-07-22 03:48:38 +0200

    Added categories page

---
+++ RazorPagesRecipes/RazorPagesRecipes/Pages/Categories/Categories.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesRecipes.Pages.Categories
+{
+    public class CategoriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
