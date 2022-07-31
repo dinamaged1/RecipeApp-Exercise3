afe4499e8049bb3fd4881c1da8b5a7a5a108f0bb
dinamaged1 <76622624+dinamaged1@users.noreply.github.com>
2022-07-31 16:48:20 +0200

    Fixed a bug related to json file

--- RazorPagesRecipes/RazorPagesRecipes/Models/Recipe.cs
+++ RazorPagesRecipes/RazorPagesRecipes/Models/Recipe.cs
@@ -10,11 +10,11 @@ namespace RazorPagesRecipes.Models
         public string Title { get; set; } = String.Empty;
         [JsonPropertyName("imagepath")]
         public string Imagepath { get; set; } = String.Empty;
-        [JsonPropertyName("instructions")]
-        public List<string> Ingredients { get; set; } = new();
-        [JsonPropertyName("categories")]
-        public List<string> Instructions { get; set; } = new();
         [JsonPropertyName("ingredients")]
+        public List<string> Ingredients { get; set; } = new();
+        [JsonPropertyName("instructions")]
+        public List<string> Instructions { get; set; } = new();
+        [JsonPropertyName("categories")]
 
         public List<string> Categories { get; set; } = new();
         public Recipe(Guid id, string title, string imagePath, List<string> instructions, List<string> ingredients, List<string> categories)
